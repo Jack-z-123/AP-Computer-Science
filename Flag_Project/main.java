8124d2c11f5d591ce9b417ed4fbef3a1b13c94d0
YunjieZhang0428 <31300529+YunjieZhang0428@users.noreply.github.com>
2017-09-07 23:05:00 -0700

    Flag Project

---
+++ Flag_Project/main.java
@@ -0,0 +1,13 @@
+public class main {
+	
+	public static void main(String[] args){
+		//creates a flag object
+		Flag a = new Flag();
+		//This sets the behavior of closing the window
+		a.setDefaultCloseOperation(a.EXIT_ON_CLOSE);
+		//This shows the flag object
+		a.setVisible(true);
+	
+	}
+
+}
